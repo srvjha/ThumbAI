17faa6f91e4d34fae6da234e47cfd500a727341b
Saurav Jha <srvtj629@gmail.com>
2026-01-31 22:32:48 +0530

    fix: adjust layout and spacing in SignUpPage and HeroSection components

--- src/app/sign-up/[[...rest]]/page.tsx
+++ src/app/sign-up/[[...rest]]/page.tsx
@@ -4,7 +4,7 @@ import { SignUp } from '@clerk/nextjs';
 
 export default function SignUpPage() {
   return (
-    <div className='flex justify-center items-center h-screen'>
+    <div className='flex justify-center items-center h-screen md:min-h-[calc(100vh+70px)]'>
       <SignUp routing='path' path='/sign-up' signInUrl='sign-in' />
     </div>
   );
